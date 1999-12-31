a167b119e781138904195b4adbbac37391b463ad
AshokShau <114943948+AshokShau@users.noreply.github.com>
2000-01-01 00:00:00 +0530

    Your commit message

--- hmm.py
+++ hmm.py
@@ -17,15 +17,23 @@ while current_date <= end_date:
         "GIT_COMMITTER_DATE": current_date.strftime("%Y-%m-%d %H:%M:%S"),
         "GIT_AUTHOR_DATE": current_date.strftime("%Y-%m-%d %H:%M:%S"),
         "GIT_AUTHOR_NAME": "AshokShau",
-        "GIT_AUTHOR_EMAIL": "Abishnoi69@outlook.com"
+        "GIT_AUTHOR_EMAIL": "114943948+AshokShau@users.noreply.github.com",  # Update this line
+        "GIT_COMMITTER_NAME": "AshokShau",  # Added line
+        "GIT_COMMITTER_EMAIL": "114943948+AshokShau@users.noreply.github.com"  # Added line
     }
 
     # Create the commit
-    subprocess.run(
+    print(f"Committing for date: {current_date.strftime('%Y-%m-%d %H:%M:%S')}")  # Debugging output
+    result = subprocess.run(
         ["git", "commit", "--date", current_date.strftime("%Y-%m-%d %H:%M:%S"), "-m", commit_message],
         env=env,
-        check=True
+        capture_output=True  # Capture output for debugging
     )
+
+    # Check if the command was successful
+    if result.returncode != 0:
+        print(f"Error committing: {result.stderr.decode()}")  # Print error message
+        break  # Exit loop on error
 
     # Increment the date by one day
     current_date += timedelta(days=1)

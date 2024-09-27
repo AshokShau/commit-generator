9b71b17c83628a8d755a6c8914af9a27c955e8f4
AshokShau <114943948+AshokShau@users.noreply.github.com>
2024-09-28 03:26:41 +0530

    done

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	fileName      = "commit_file.txt" // Single file for all commits
+	fileName      = "commit_file.txt"
 	startDate     = "2024-09-24"
 	endDate       = "2024-09-28"
 	commitMessage = "Test-Automation-Commit"
@@ -16,54 +16,42 @@ const (
 	authorName    = "AshokShau"
 	authorEmail   = "114943948+AshokShau@users.noreply.github.com"
 )
-		
+
+// main is the entry point of the program. It generates multiple git commits
+// for a specified date range and pushes them to a remote repository.
 func main() {
 	start, err := time.Parse("2006-01-02", startDate)
 	if err != nil {
 		logError("Error parsing start date:", err)
 		return
 	}
-	logInfo("Start date parsed successfully:", start)
-
 	end, err := time.Parse("2006-01-02", endDate)
 	if err != nil {
 		logError("Error parsing end date:", err)
 		return
 	}
-	logInfo("End date parsed successfully:", end)
 
-	currentDate := start
-
-	// Create or open the single commit file
 	if err := os.WriteFile(fileName, []byte("Commits:\n"), 0644); err != nil {
 		logError("Error writing initial file:", err)
 		return
 	}
-	logInfo("Initial commit file created:", fileName)
 
-	for currentDate.Before(end) || currentDate.Equal(end) {
+	for currentDate := start; !currentDate.After(end); currentDate = currentDate.AddDate(0, 0, 1) {
 		for i := 0; i < commitsPerDay; i++ {
 			content := fmt.Sprintf("This is commit number %d for %s.\n", i+1, currentDate.Format("2006-01-02"))
 			if err := appendToFile(fileName, content); err != nil {
 				logError("Error appending to file:", err)
 				return
 			}
-			logInfo("Appended to file:", content)
-
 			if err := gitAdd(fileName); err != nil {
 				logError("Error adding file:", err)
 				return
 			}
-			logInfo("File added to git:", fileName)
-
 			if err := gitCommit(currentDate, i); err != nil {
 				logError("Error committing:", err)
 				return
 			}
-			logInfo("Commit created for date:", currentDate.Format("2006-01-02"))
 		}
-		currentDate = currentDate.AddDate(0, 0, 1)
-		logInfo("Moving to next date:", currentDate.Format("2006-01-02"))
 	}
 
 	if err := gitPush(); err != nil {
@@ -73,22 +61,23 @@ func main() {
 	}
 }
 
+// appendToFile appends the given content to the specified file.
 func appendToFile(filename, content string) error {
 	file, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
-
 	_, err = file.WriteString(content)
 	return err
 }
 
+// gitAdd stages the specified file for commit.
 func gitAdd(filename string) error {
-	cmd := exec.Command("git", "add", filename)
-	return cmd.Run()
+	return exec.Command("git", "add", filename).Run()
 }
 
+// gitCommit creates a git commit with the specified date and commit index.
 func gitCommit(currentDate time.Time, commitIndex int) error {
 	env := []string{
 		fmt.Sprintf("GIT_COMMITTER_DATE=%s", currentDate.Format("2006-01-02 15:04:05")),
@@ -98,22 +87,23 @@ func gitCommit(currentDate time.Time, commitIndex int) error {
 		fmt.Sprintf("GIT_COMMITTER_NAME=%s", authorName),
 		fmt.Sprintf("GIT_COMMITTER_EMAIL=%s", authorEmail),
 	}
-
 	cmd := exec.Command("git", "commit", "--date", currentDate.Format("2006-01-02 15:04:05"), "-m", fmt.Sprintf("%s - %d", commitMessage, commitIndex+1))
 	cmd.Env = env
-
 	return cmd.Run()
 }
 
+// gitPush pushes the committed changes to the remote repository.
 func gitPush() error {
-	cmd := exec.Command("git", "push", "origin", "master")
-	return cmd.Run()
+	return exec.Command("git", "push", "origin", "master").Run()
 }
 
+// logInfo logs informational messages to the console.
 func logInfo(v ...interface{}) {
 	fmt.Println("[INFO]", fmt.Sprint(v...))
 }
 
+// logError logs error messages to the console.
 func logError(v ...interface{}) {
 	fmt.Println("[ERROR]", fmt.Sprint(v...))
 }
+```
